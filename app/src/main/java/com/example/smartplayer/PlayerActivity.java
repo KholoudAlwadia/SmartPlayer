99d97646462e92cc90803fb13cb081dfa38698a3
kholoud707 <khokho70729@gmail.com>
2021-04-02 19:48:18 +0300

    Initial commit

---
+++ app/src/main/java/com/example/smartplayer/PlayerActivity.java
@@ -0,0 +1,14 @@
+package com.example.smartplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+    }
+}
